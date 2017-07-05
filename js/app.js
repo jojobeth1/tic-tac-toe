059f235da493391767f2207af641edc6c8c01bf9
jojobeth1@gmail.com <jojobeth1@gmail.com>
2017-07-04 17:35:12 -0700

    added js for tic tac toe game

--- js/app.js
+++ js/app.js
@@ -2,5 +2,24 @@
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
+  var ticTacToe = function () {
+//Players
+    var xPlayer = X;
+    var oPlayer = O;
+    //Player x goes first
+    CurrentPlayer: X;
+    /**
+		Switch between X player and O player.
+		*/
+		this.swapCurrentPlayer = function() {
+			if(CurrentPlayer == xPlayer) {
+				CurrentPlayer = oPlayer;
+			}
+			$("#messageToPlayers").text("Player "+CurrentPlayer+"'is up.");
+		}
+  // Winning patterns
+		waysToWin = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]];
+
+  };
 
 });
